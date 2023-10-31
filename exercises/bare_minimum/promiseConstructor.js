53ef43187774de8b168fc8f32a2f2b02538bd394
Justin Cordova <justcord10@gmail.com>
2023-10-31 12:01:22 -0700

    Completed bare_minimum/promisification.js exercises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,15 +7,33 @@
 var fs = require('fs');
 var request = require('needle');
 var Promise = require('bluebird');
-
+var callbacks = require('./callbackReview');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        var lines = data.split('\n');
+        resolve(lines[0]);
+      }
+    });
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise ((resolve, reject)=> {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
